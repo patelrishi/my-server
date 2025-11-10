21090255d9389c65884010284b008f6014dc54c7
patelrishi <patelsai9696@gmail.com>
2025-11-10 10:46:07 +0530

    get method

--- routes/students.js
+++ routes/students.js
@@ -16,5 +16,19 @@ router.post('/student', async function(req,res,next){
         res.send(err.message)
     }
 })
+
+router.get('/get-student', async function(req,res,next){
+    try{
+    const MongoClient = mongodb.MongoClient
+    const server = await MongoClient.connect('mongodb+srv://nit:nit@cluster0.26gts.mongodb.net')
+    const db = server.db('sms')
+    const collection = db.collection('students')
+    const data = await collection.find().toArray()
+    res.send(data)
+    
+    } catch(err){
+        res.send(err.message)
+    }
+})
 //http://localhost:3030/std/students
 module.exports = router;
